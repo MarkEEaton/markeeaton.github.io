e6b70799e67e879275d87e178a3583a477e4154d
Mark Eaton <MarkEEaton@users.noreply.github.com>
2022-08-12 02:47:49 -0400

    break out focus.js

---
+++ focus.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("img").mouseenter(
+        function() {
+            $(this).css("opacity", "0.3")
+            $(this).next().css("bottom", this.height / 2 - 20)
+            console.log($(this).next().hasClass("except"))
+            if ($(this).next().hasClass("except") == true) {
+                $(this).next().css("left", $(this).parent().width() / 2 - 30);
+            }
+            else {
+                $(this).next().css("left", $(this).parent().width() / 2 - 68);
+            }
+            $(this).next().show()
+        });
+    $(".container").mouseleave(
+        function() {
+            $("img").css("opacity", "1");
+            $("img").next().hide()
+        }
+    );
+}); 
