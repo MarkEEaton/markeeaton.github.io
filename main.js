6ec78e4f0764f0fd825b5c99ce7b99a37251fe17
Mark Eaton <MarkEEaton@users.noreply.github.com>
2023-07-02 01:56:46 -0400

    move data into main.js

--- main.js
+++ main.js
@@ -17,13 +17,95 @@ const projectsComponent = {
 const { createApp } = Vue;
 
 const app = createApp({
-  data() {
-    projects1: Seed.projectsComponent1,
-    projects2: Seed.projectsComponent2
-  },
-  components: {
-    'projects-component': projectsComponent
-  }
-});
+	data() {
+		return {
+			projectsComponent1: [
+				{
+					id: 1,
+					title: "Open Journal Matcher",
+					subhead: "A tool to find relevant open access journals",
+					image: "static/images/ojm2.png",
+					alt: "A list of results from the Open Journal Matcher",
+					url: "https://github.com/markeeaton/open-journal-matcher"
+				},
+				{
+					id: 2,
+					title: "GH-Index",
+					subhead: "Calculates h-index, but for open source contributions",
+					image: "static/images/ghindex.png",
+					alt: "Screenshot of the gh-index webpage",
+					url: "https://github.com/markeeaton/gh-index"
+				},
+				{
+					id: 3,
+					title: "A Nonsensical Ab\u00E9c\u00E9daire",
+					subhead: "A novel for NaNoGenMo 2021",
+					image: "static/images/nanogenmo2021.png",
+					alt: "A few lines of the novel A nonsensical abécédaire",
+					url: "https://github.com/markeeaton/nanogenmo2021" 
+				},
+				{
+					id: 9,
+					title: "Index Visualizer",
+					subhead: "Visually explore a back-of-the-book index",
+					image: "static/images/IndexViz.png",
+					alt: "A collapsible tree visualization of an index",
+					url: "https://github.com/markeeaton/index-visualizer"
+				},
+				{
+					id: 5,
+					title: "Wandering Around America with Mark Twain",
+					subhead: "A novel for NaNoGenMo 2020",
+					image: "static/images/twain.png",
+					alt: "A few lines of the novel Wandering Around Ameria with Mark Twain",
+					url: "https://github.com/markeeaton/nanogenmo2020"
+				}];
+			projectsComponent2: [
+				{
+					id: 6,
+					title: "Why Bot",
+					subhead: "Asks the armchair philosopher's favorite question: Why?",
+					image: "static/images/why3.png",
+					alt: "A post by Why Bot",
+					url: "https://botsin.space/@why/",
+				},
+				{
+					id: 7,
+					title: "mastodon.ocert.at",
+					subhead: "I'm running a Mastodon server!",
+					image: "static/images/moa.png",
+					alt: "A screenshot from mastodon.ocert.at",
+					url: "https://mastodon.ocert.at",
+				},
+				{
+					id: 8,
+					title: "Fictograph",
+					subhead: "Plot the awesomeness of an author's work over time",
+					image: "static/images/auster-fictograph.png",
+					alt: "A chart of the popularity of Paul Auster's works",
+					url: "https://github.com/markeeaton/fictograph"
+				},
+				{
+					id: 9,
+					title: "Tweets by Month",
+					subhead: "Graph your number of tweets per month",
+					image: "static/images/tbm.png",
+					alt: "A bar chart of tweets by month",
+					url: "https://github.com/markeeaton/Tweets-by-month"
+				},
+				{
+					id: 10,
+					title: "SeeCollections",
+					subhead: "Visualize your collections with the Primo API",
+					image: "static/images/PrimoViz.png",
+					alt: "Bubble chart of book holdings",
+					url: "https://github.com/markeeaton/Primo-Visualization"
+				}
+			];
+		},
+		components: {
+			'projects-component': projectsComponent
+		}
+	});
 app.mount('#app');
 
